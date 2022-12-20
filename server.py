f616dbc9dddcdbe730478f55afc917acc55d294c
mdapim <mdapim@outlook.com>
2022-12-20 17:33:34 +0000

    added cors

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask, current_app, jsonify, request
 from UserAccounts.UserAccountsAPI import *
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, origins='https://localhost:3000')
+
 @app.route('/create_user', methods = ['GET','POST'])
 def getting_user():
     if(request.json == 'GET'):  
