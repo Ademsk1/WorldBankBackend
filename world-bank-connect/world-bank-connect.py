ac03a9c6d2f3ee747e6900377db1e3cc59434184
Ademsk1 <adam.illoul24@gmail.com>
2022-12-20 15:48:27 +0000

    Connection made to world bank database with secure passwords

--- world-bank-connect/world-bank-connect.py
+++ world-bank-connect/world-bank-connect.py
@@ -8,21 +8,26 @@ load_dotenv()
 app = Flask(__name__)
 pw = os.getenv('GCP_PROJECT_ID')
 print(pw)
+WB_DBNAME = os.getenv('WB_DBNAME')
+WB_HOST = os.getenv('WB_HOST')
+WB_PASSWORD = os.getenv('WB_PASSWORD')
+WB_USERNAME = os.getenv('WB_USERNAME')
 
 
 def get_bank_connection():
+
     try:
-        conn = psycopg2.connect(
-            f"")
+        conn = conn = psycopg2.connect(
+            f"dbname={WB_DBNAME} user={WB_USERNAME} host={WB_HOST} port = 5432 password={WB_PASSWORD}")
         print('connected')
         return conn
     except:
         return False
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def main():
-    if request.method == 'POST':
+    if request.method == 'GET':
         data = request.data
         conn = get_bank_connection()
         query = "SELECT * fROM public.indicators WHERE countrycode IN ('ARB','GBR') AND indicatorcode='EN.ATM.CO2E.GF.ZS';"
