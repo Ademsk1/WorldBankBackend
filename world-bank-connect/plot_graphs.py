0a2bb7a23fdce9f32443a9f9fce1223665d129fd
Ademsk1 <adam.illoul24@gmail.com>
2022-12-21 14:25:03 +0000

    added in a plotting feature to save the plots in plots file, and also scale to the correct year range

---
+++ world-bank-connect/plot_graphs.py
@@ -0,0 +1,34 @@
+from matplotlib.figure import Figure
+from textwrap import wrap
+
+
+def plot_graph(results, indicator):
+    indicator = indicator[0]
+    country_list = []
+    dict_data = {}
+    fig = Figure()
+    ax = fig.subplots()
+    for data in results:
+        country = data['countryname']
+        value = data['value']
+        year = data['year']
+        if country not in country_list:
+            country_list.append(data['countryname'])
+            dict_data[country] = []
+        else:
+            dict_data[country].append(
+                [year, value])
+    x = []
+    y = []
+    for key in dict_data:
+        for datapoint in dict_data[key]:
+            x.append(datapoint[0])
+            y.append(datapoint[1])
+        ax.plot(x, y, label=key)
+        ax.set_xlabel('Year')
+        ax.set_ylabel('\n'.join(wrap(indicator, 40)))
+        x = []
+        y = []
+    ax.legend()
+    ax.grid()
+    fig.savefig('./plots/plot.png')
--- world-bank-connect/world-bank-connect.py
+++ world-bank-connect/world-bank-connect.py
@@ -4,6 +4,7 @@ import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
 from flask_cors import CORS
+from plot_graphs import plot_graph
 import json
 
 load_dotenv()
@@ -27,7 +28,9 @@ def validate_input(data):
 def get_params(search):
     country_params = tuple(search['country'])
     indicator_params = tuple(search['indicator'])
-    params = tuple([country_params, indicator_params])
+    start_date = search['range'][0]
+    end_date = search['range'][1]
+    params = tuple([country_params, indicator_params, start_date, end_date])
     return params
 
 
@@ -63,12 +66,15 @@ def search():
                      'LENGTH=0': 'Please select countries and indicator(s)'}
     if request.method == 'POST':
         inp = jsonify({'country': ['Afghanistan', 'Albania'], 'indicator': [
-                      'Merchandise imports from developing economies in South Asia (% of total merchandise imports)']})
+                      'Merchandise imports from developing economies in South Asia (% of total merchandise imports)'], 'range': ['1964', '1999']})
         search = inp.json
         error_message = validate_input(search)
-        query = "SELECT countryname,value,year FROM public.indicators WHERE countryname IN %s AND indicatorname IN %s"
+        start = search['range'][0]
+        end = search['range'][1]
+        query = "SELECT countryname,value,year FROM public.indicators WHERE countryname IN %s AND indicatorname IN %s AND year BETWEEN %s AND %s"
         params = get_params(search)
         results = query_bank_db(query, params)
+        plot_graph(results, search['indicator'])
         response = {'results': results, 'errors': error_message}
         return jsonify(response)
 
