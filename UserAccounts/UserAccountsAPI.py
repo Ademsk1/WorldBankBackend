e3f389c78fdcdf51c383146758cfd6b6e7eccc3e
mdapim <mdapim@outlook.com>
2022-12-21 09:18:53 +0000

    Log in APIs for creating and logging in user

--- UserAccounts/UserAccountsAPI.py
+++ UserAccounts/UserAccountsAPI.py
@@ -6,10 +6,39 @@ from UserAccounts.connections import db_select, get_db_user_connection
 conn_user_db = get_db_user_connection()
 
 
-def create_user(data): #check database first
-    check_stories2 = db_select(conn_user_db, 'select * from user_types')
-    return jsonify(check_stories2)
+def create_user(data):
+    check_stories1 = db_select(conn_user_db, 'select * from user_table')
+    try:
+        if not any(dictionary.get('username') == data[0]['name'] and compare_hashed_passwords(data[0]['password'], dictionary.get('salt'), dictionary.get('password')) for dictionary in check_stories1):
+            hashed_result = (create_hash_password(data[0]['password']))
+            send = db_select(conn_user_db, 'insert into user_table (username, password, type_id, salt) values (%s, %s, 1, %s) returning 1', ((data[0]['name']),(hashed_result[0]),(hashed_result[1])))
+        else:
+            return 'user is already in database'
 
-def log_in(): #check database first
-    check_stories2 = db_select(conn_user_db, 'select * from user_types')
-    return jsonify(check_stories2)
+        check_stories2 = db_select(conn_user_db, 'select * from user_table')
+        return jsonify(check_stories2)
+    except:
+        'Error creating account'
+
+def get_user_data(data):
+    try:
+        check_stories1 = db_select(conn_user_db, 'select * from user_table')
+        for user in check_stories1:
+            if(user['username'] == data[0]['name'] and compare_hashed_passwords(data[0]['password'], user.get('salt'), user.get('password'))):
+                check_stories2 = db_select(conn_user_db, 'select * from user_table where username=%s and password=%s', ((user['username']),(user['password'])))
+                return jsonify(check_stories2)
+        return 'user was not found'
+    except:
+        'Error Fetching User'
+
+
+
+def create_hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('UTF-8'), salt)
+    return([hashed.decode('UTF-8'),salt.decode('UTF-8')])
+
+def compare_hashed_passwords(inputted_password, salt, saved_password):
+    return bcrypt.checkpw(inputted_password.encode('UTF-8'), saved_password.encode('UTF-8'))
+
+
--- server.py
+++ server.py
@@ -6,12 +6,21 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins='https://localhost:3000')
 
-@app.route('/create_user', methods = ['GET','POST'])
-def getting_user():
-    if(request.json == 'GET'):  
-        return log_in()
-    elif(request.json == 'POST'):
-        return create_user()
+@app.route('/get_user', methods = ['GET', 'POST'])
+def getting_user(): 
+        if(request.method == 'POST'):
+            data = request.json
+            return get_user_data(data)
+
+
+
+
+@app.route('/create_user', methods= ['GET','POST'])
+def creating_user():
+    if(request.json == 'POST'):
+        data = request.json
+        return create_user(data)
+
 
 
 
